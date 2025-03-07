cd6e2eff401086462374f404667d8f7184d2cd42
Pietro dos Santos Menezes <pietro.dsantos77@gmail.com>
2025-03-06 23:03:08 -0300

    fix: exclude dist

--- dist/application/config/env.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = void 0;
-exports.env = {
-    JWT_SECRET: process.env.JWT_SECRET || 'sua_chave_secreta',
-};
